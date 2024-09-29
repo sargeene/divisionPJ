20b3e98ce0b33c1aead1bcebde44943799aed7d3
sargeene <sargeene@gmail.com>
2024-09-29 17:05:33 +0100

    updated

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
    subscription_id = var.subscription-id
   # client_id       = "your_client_id"        # Optional if using environment variables
   # client_secret   = "your_client_secret"    # Optional if using environment variables
-  # tenant_id       = "9a09f19b-c5d0-43c1-a593-c35c689354c3"        # Optional if using environment variables
+  # tenant_id       = "your_tenant_id"        # Optional if using environment variables
 }
 provider "random" {
   # Configuration options
